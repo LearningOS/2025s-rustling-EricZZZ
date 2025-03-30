d1591fda6d3f0b41a70458139b9beee2ab70ba8d
EricZzz <biggogten@gmail.com>
2025-03-30 19:25:01 +0800

    first commit

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -30,19 +28,23 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1 = tx.clone();
+    let tx2 = tx;
 
     thread::spawn(move || {
         for val in &qc1.first_half {
+            let mut value = tx1.send(*val);
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            value.unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
         for val in &qc2.second_half {
+            let mut value = tx2.send(*val);
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            value.unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
